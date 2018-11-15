12523000146a140ed1a4119c923f5fa08fb942fd
Braden Farmer <farmerbb@gmail.com>
2018-11-15 15:56:00 -0700

    Minor fixes

--- app-blue/src/main/java/com/example/blue/ExampleActivity1.kt
+++ app-blue/src/main/java/com/example/blue/ExampleActivity1.kt
@@ -7,7 +7,7 @@ import android.content.IntentFilter
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.example_activity_1.*
+import kotlinx.android.synthetic.main.example_activity.*
 
 class ExampleActivity1: AppCompatActivity() {
 
@@ -20,7 +20,7 @@ class ExampleActivity1: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.example_activity_1)
+        setContentView(R.layout.example_activity)
     }
 
     override fun onStart() {
--- app-blue/src/main/java/com/example/blue/ExampleActivity2.kt
+++ app-blue/src/main/java/com/example/blue/ExampleActivity2.kt
@@ -2,13 +2,13 @@ package com.example.blue
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.example_activity_2.*
+import kotlinx.android.synthetic.main.example_activity.*
 
 class ExampleActivity2: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.example_activity_2)
+        setContentView(R.layout.example_activity)
 
         val textToDisplay = intent.getStringExtra("text_to_display")
         textView.text = textToDisplay
